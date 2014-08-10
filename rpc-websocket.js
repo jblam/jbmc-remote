3817e9e7e5535241113ad026afd67cf36178e4f2
jblam <jeremy.blamey@gmail.com>
2014-08-10 17:31:21 +1000

    Decided on websockets; removed XHR
    Websocket and RPC constructors are now inside their own closures.

--- rpc-websocket.js
+++ rpc-websocket.js
@@ -1,66 +1,131 @@
-const commandRegister = {};
-function routeCommand(response) {
-  var data;
-  try {
-    data = JSON.parse(response.data); 
-  } catch (e) {
-    console.error("Could not parse response " + response.data);
-  }
-  if (!data) { return; }
-  
-  if (data.hasOwnProperty("id")) {
-    var cmd = commandRegister[data.id];
-    if (!cmd) {
-      console.error("Could not find corresponding command for data", data);
-      return;
-    }
+var RPC = (function RPC_closure() {
+	"use strict";
+	
+	// retain references to commands while awaiting response from the server
+	const commandRegister = {};
+	const notificationHandlers = {};
+	
+	// Adds a handler which will be called-back when the notification is received
+	function addNotificationHandler(notification, handler) {
+		var handlerList = notificationHandlers[notification];
+		if (!handlerList) {
+			notificationHandlers[notification] = handlerList = [];
+		}
+		handlerList.push(handler);
+	}
+	
+	// Removes the given handler from the notification listing
+	function removeNotificationHandler(notification, handler) {
+		var handlerList = notificationHandlers[notification];
+		if (handlerList) {
+			notificationHandlers[notification] = handlerList.filter(x => x != handler);
+		}
+	}
+	
+	// for a given server response, locate its command and resolve/reject as appropriate
+	// response is a WebSocket MessageEvent
+	function routeCommand(response) {
+	  var data;
+	  try {
+		data = JSON.parse(response.data); 
+	  } catch (e) {
+		console.error("Could not parse response " + response.data);
+	  }
+	  if (!data) { return; }
+	  
+	  if (data.hasOwnProperty("id")) {
+	    // if the response has an "id", it is a response to a previously-issued command
+		var cmd = commandRegister[data.id];
+		if (!cmd) {
+		  console.error("Could not find corresponding command for data", data);
+		  return;
+		}
 
-    if (data.result) {
-      cmd.resolve(data.result);
-    } else if (data.error) {
-      cmd.reject(data.error);
-    } else {
-      cmd.reject(data);
-    }
-  } else {
-    // notification
-    var handler = commandRegister[data.method];
-    if (handler) { handler(data); }
-    else {
-      console.info("Unhandled notification", data);
-    }
-  }
-}
+		if (data.result) {
+		  cmd.resolve(data.result);
+		} else if (data.error) {
+		  cmd.reject(data.error);
+		} else {
+		  // this is a very-unexpected state
+		  console.error("RPC response had neither \"result\" nor error");
+		  cmd.reject(data);
+		}
+	  } else {
+		// if the response has no "id", it's a push-notification
+		var handler = notificationHandlers[data.method];
+		if (handler) { handler(data); }
+		else {
+		  // TODO: implement debug levels
+		  console.info("Unhandled notification", data);
+		}
+	  }
+	}
 
-function getWebsocket() {
-  const url = new URL("/jsonrpc", location);
-  url.port = 9090;
-  url.protocol = "ws";
-  var webSocket = new Promise(function(resolve, reject) {
-    var ws = new WebSocket(url.href);
-    ws.onopen = () => resolve(ws);
-    ws.onerror = () => reject(ws);
-    ws.onmessage = evt => routeCommand(evt);
-  });
-  webSocket.then(ws => window.addEventListener('beforeunload', () => ws.close()));
-  return webSocket;
-}
+	// returns a promise for a websocket
+	function getWebsocket() {
+	  // The JSON-RPC endpoint path and protocol are not configurable in XBMC
+	  const url = new URL("/jsonrpc", location);
+	  url.port = 9090; // TODO: handle configurable TCP port in XBMC
+	  url.protocol = "ws";
+	  var webSocket = new Promise(function(resolve, reject) {
+		var ws = new WebSocket(url.href);
+		ws.onopen = () => resolve(ws);
+		ws.onerror = () => reject(ws);
+		// TODO: sensible handling of ws.onclose
+		// each incoming message should be routed to the client-issued command
+		// which prompted it, or should trigger a notification handler
+		ws.onmessage = evt => routeCommand(evt);
+	  });
+	  // attempt to close the websocket nicely, if at all possible
+	  // in Firefox, this typically fails
+	  webSocket.then(ws => window.addEventListener('beforeunload', () => ws.close()));
+	  return webSocket;
+	}
 
-let wp = getWebsocket();
+	var wp = getWebsocket();
 
-function sendCommand(cmd) {
-  // TODO: handle bare string commands
-  var resolver, rejecter;
-  var p = new Promise((resolve, reject) => {
-    resolver = data => resolve(data);
-    rejecter = error => reject(error);
-  });
-  commandRegister[cmd.id] = {resolve:resolver, reject:rejecter};
-  wp.then(ws => ws.send(JSON.stringify(cmd)));
-  return p;
-}
+	// sends the given command; the command must be either a string (in which
+	// case it is converted to a JSON-RPC object), or a valid JSON-RPC object
+	// returns a promise which is resolved with the result if the command succeeds,
+	// or rejected with the error if the command fails
+	function sendCommand(cmd) {
+	  // ensure that cmd is a valid command
+	  if ((typeof cmd) == "string") {
+		cmd = new RpcObject(cmd);
+	  }
+	  if (!cmd.hasOwnProperty("id")) {
+		throw Error("Attempted to send command without an ID");
+	  }
+	  if (cmd.jsonrpc != "2.0") {
+		throw Error("Cannot send non-JSONRPC/2.0 commands");
+	  }
+	  
+	  // acquire external references to the promise resolve/reject methods
+	  var resolver, rejecter;
+	  var p = new Promise((resolve, reject) => {
+		resolver = data => resolve(data);
+		rejecter = error => reject(error);
+	  });
+	  
+	  // register the above references in the command register
+	  commandRegister[cmd.id] = {resolve:resolver, reject:rejecter};
+	  
+	  // send the command, and return the promise
+	  wp.then(ws => ws.send(JSON.stringify(cmd)));
+	  return p;
+	}
 
-function doTests() {
-  sendCommand(new RpcObject("JSONRPC.Introspect")).then(x => console.log("Introspection: ", x));
-  sendCommand(new RpcObject("VideoLibrary.Getasdfadsf")).catch(x => console.log("Success, error caught: " + x.message));
-}
+	// couple of basic tests to verify that real commands resolve with their response,
+	// and illegal commands reject with an error
+	function doTests() {
+	  sendCommand(new RpcObject("JSONRPC.Introspect")).then(x => console.log("Introspection: ", x));
+	  sendCommand(new RpcObject("VideoLibrary.Getasdfadsf")).catch(x => console.log("Success, error caught: " + x.message));
+	}
+	
+	// expose public methods
+	return {
+		addNotificationHandler:addNotificationHandler,
+		removeNotificationHandler:removeNotificationHandler,
+		send:sendCommand,
+	};
+}());
--- rpc.js
+++ rpc.js
@@ -1,17 +1,19 @@
-"use strict";
-const rpcLogNone = 0;
-const rpcLogError = 1;
-const rpcLogSuccess = 2;
+var RpcObject = (function RpcConstructor_closure() {
+	"use strict";
+	const rpcLogNone = 0;
+	const rpcLogError = 1;
+	const rpcLogSuccess = 2;
 
-var rpcLogLevel = 1;
+	function logPromise(x) { console.log(x); return x; }
+	function errorPromise(x) { console.error(x); throw x; }
 
-function logPromise(x) { console.log(x); return x; }
-function errorPromise(x) { console.error(x); throw x; }
-
-var rpcId = 1;
-function RpcObject(method, params) {
-  this.method = method;
-  if (params) { this.params = params; }
-  this.id = rpcId++;
-  this.jsonrpc = "2.0";
-}
+	var rpcId = 1;
+	var output = function RpcObject(method, params) {
+	  this.method = method;
+	  if (params) { this.params = params; }
+	  this.id = rpcId++;
+	  this.jsonrpc = "2.0";
+	};
+	output.LogLevel = 1;
+	return output;
+}());
