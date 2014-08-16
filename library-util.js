3072eb06ff0f13d5ae58c304e37770d1708cd753
jblam <jeremy.blamey@gmail.com>
2014-08-16 10:28:16 +1000

    Added very rough concept for adding library elements to the main view

--- library-util.js
+++ library-util.js
@@ -1,7 +1,9 @@
 var exampleImage = "image://http%3a%2f%2fimage.tmdb.org%2ft%2fp%2foriginal%2fpQcSfSu4hy4AJ1KjjTxXKBQdMzE.jpg/"
 
-function acquireImage(imageUrl) {
-  var innerUrl = /^image:\/\/(.*)\/$/.exec(imageUrl)[1];
+var formatImageUri = uri => /^image:\/\/(.*)\/$/.exec(uri)[1];
+
+function acquireImage(imageUri) {
+  var innerUrl = formatImageUri(imageUri);
   var output = new Image();
   output.src = decodeURIComponent(innerUrl);
   return output;
---
+++ media-library.js
@@ -0,0 +1,16 @@
+// library ui code
+"use strict";
+var entryTemplate;
+var mainListing;
+var defaultImageUri = "http://placehold.it/50x50";
+document.addEventListener('DOMContentLoaded', evt => {
+	entryTemplate = document.getElementById("listing-template");
+	mainListing = document.querySelector(".main-listing");
+}, false);
+
+function createListingEntry(imageUri, title, description) {
+	var clone = document.importNode(entryTemplate.content, true);
+	clone.querySelector(".listing-thumb").src = imageUri || defaultImageUri;
+	clone.querySelector(".listing-title").textContent = title;
+	mainListing.appendChild(clone);
+}
