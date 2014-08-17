e688f31b9cb28bbd50f628855395444f799235f6
jblam <jeremy.blamey@gmail.com>
2014-08-17 12:17:09 +1000

    Tweaked presentation of media library elements

--- media-library.js
+++ media-library.js
@@ -12,5 +12,6 @@ function createListingEntry(imageUri, title, description) {
 	var clone = document.importNode(entryTemplate.content, true);
 	clone.querySelector(".listing-thumb").src = imageUri || defaultImageUri;
 	clone.querySelector(".listing-title").textContent = title;
+	clone.querySelector(".listing-description").textContent = description || "";
 	mainListing.appendChild(clone);
 }
