fb6d348d6c84001ec574c754cdcd0ed9e1a41502
jblam <jeremy.blamey@gmail.com>
2014-09-18 21:32:10 +1000

    Proof-of-concept: "music" button will add all albums to the main view

--- media-library.js
+++ media-library.js
@@ -6,6 +6,7 @@ var defaultImageUri = "http://placehold.it/50x50";
 document.addEventListener('DOMContentLoaded', evt => {
 	entryTemplate = document.getElementById("listing-template");
 	mainListing = document.querySelector(".main-listing");
+	document.getElementById("media-music").addEventListener("click", showAlbums, false);
 }, false);
 
 function createListingEntry(imageUri, title, description) {
@@ -14,4 +15,12 @@ function createListingEntry(imageUri, title, description) {
 	clone.querySelector(".listing-title").textContent = title;
 	clone.querySelector(".listing-description").textContent = description || "";
 	mainListing.appendChild(clone);
+}
+
+function showAlbums() {
+	var pAlbum = RPC.send("AudioLibrary.GetAlbums");
+	pAlbum.then(obj => alert(obj.albums.length));
+	pAlbum.then(obj => obj.albums.forEach(
+		a => createListingEntry(defaultImageUri, a.label, "No description")
+	));
 }
