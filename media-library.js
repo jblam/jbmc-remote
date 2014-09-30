e934f26cb811615a075a24789c6790218840f784
jblam <jeremy.blamey@gmail.com>
2014-09-30 19:47:31 +1000

    More proof-of-concept library functions
    - gracefully handle if the library is empty
    - PoC for event handler on templated items

--- media-library.js
+++ media-library.js
@@ -6,7 +6,12 @@ var defaultImageUri = "http://placehold.it/50x50";
 document.addEventListener('DOMContentLoaded', evt => {
 	entryTemplate = document.getElementById("listing-template");
 	mainListing = document.querySelector(".main-listing");
+	mainListing.addEventListener("click", function(evt) {
+		alert(evt.target.textContent);
+	}, false);
 	document.getElementById("media-music").addEventListener("click", showAlbums, false);
+	document.getElementById("media-tv").addEventListener("click", showTV, false);
+	document.getElementById("media-movies").addEventListener("click", showMovies, false);
 }, false);
 
 function createListingEntry(imageUri, title, description) {
@@ -16,11 +21,30 @@ function createListingEntry(imageUri, title, description) {
 	clone.querySelector(".listing-description").textContent = description || "";
 	mainListing.appendChild(clone);
 }
-
+function showTV() {
+	var pTV = RPC.send("VideoLibrary.GetTVShows");
+	pTV.then(obj => {
+		if (!obj.limits.total) { console.log("no movies"); return; }
+		obj.tvshows.forEach(
+			a => createListingEntry(defaultImageUri, a.label, "No description")
+		);
+	});
+}
+function showMovies() {
+	var pMovies = RPC.send("VideoLibrary.GetMovies");
+	pMovies.then(obj => {
+		if (!obj.limits.total) { console.log("no TV"); return; }
+		obj.movies.forEach(
+			a => createListingEntry(defaultImageUri, a.label, "No description")
+		);
+	});
+}
 function showAlbums() {
 	var pAlbum = RPC.send("AudioLibrary.GetAlbums");
-	pAlbum.then(obj => alert(obj.albums.length));
-	pAlbum.then(obj => obj.albums.forEach(
-		a => createListingEntry(defaultImageUri, a.label, "No description")
-	));
+	pAlbum.then(obj => {
+		if (!obj.limits.total) { console.log("no albums"); return; }
+		obj.albums.forEach(
+			a => createListingEntry(defaultImageUri, a.label, "No description")
+		)
+	});
 }
